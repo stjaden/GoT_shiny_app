728cb64a69f694b0ca39ef871c91c812e0fb890d
melchera <melcher.austin@gmail.com>
2019-03-02 17:13:52 -0800

    second dynamic graph

--- app.R
+++ app.R
@@ -50,11 +50,8 @@ ui <- fluidPage(
                           selectInput("house2_explore",
                                       "Explore House 2 Battle Stats:",
                                       choices = c("Stark","Lannister","Baratheon","Tully","Greyjoy","Frey","Bolton","Karstark","Mormont","Glover","Tyrell")
-                          ),
-                          selectInput("house3_explore",
-                                      "Explore House 3 Battle Stats",
-                                      choices = c("Stark","Lannister","Baratheon","Tully","Greyjoy","Frey","Bolton","Karstark","Mormont","Glover","Tyrell")
                           )
+                          
                         ),
                         
                         # PLACEHOLDER: Show a plot of the generated distribution
@@ -78,7 +75,7 @@ ui <- fluidPage(
                           # Battle type stats graphs
                           fluidRow(
                             column(6, plotOutput("battle_type_hist1")),
-                            column(6, wellPanel(p("Battle Stats 2")))
+                            column(6, plotOutput("battle_type_hist2"))
                             ),
                           
                           
@@ -183,11 +180,29 @@ server <- function(input, output) {
     
   })
   
-  output$scatter <- renderPlot({
+  #Create battle type histogram 2 on Explore tab based on house 1 selection
+  output$battle_type_hist2 <- renderPlot({
     
-    ggplot(faithful, aes(x = waiting, y = eruptions)) +
-      geom_point(color = input$scattercolor) +
-      geom_smooth(method = "lm", se = FALSE)
+    battle_type_data <- house_stats %>%
+      filter(house == input$house2_explore)
+    
+    
+    ggplot(battle_type_data, aes(x=battle_type))+
+      geom_bar(aes(fill= outcome), position = "stack") +
+      theme_classic() +
+      theme(legend.position = "", 
+            title = element_text(size=20, face = "bold"),
+            plot.title = element_text(hjust = 0.5),
+            axis.text.x = element_text(size = 15),
+            axis.text.y = element_text(size = 15)
+      ) +
+      scale_fill_manual(values = c("red", "darkgreen")) +
+      scale_y_continuous(expand = c(0, 0), limits = c(0, 6), breaks = c(0,1,2,3,4,5)) +
+      ylab("")  +
+      xlab("") +
+      ggtitle(input$house2_explore)+
+      coord_flip()
+    
     
   })
   
