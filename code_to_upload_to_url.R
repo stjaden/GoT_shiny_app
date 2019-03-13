8bf4407fb1eb764534b9f121f726e2c03fb77e1b
melchera <melcher.austin@gmail.com>
2019-03-13 12:16:51 -0700

    upload to url

---
+++ code_to_upload_to_url.R
@@ -0,0 +1,6 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='whitewalkerwipeout',
+                          token='C7B31432A6E5045D4A35B3DCF64C5110',
+                          secret='Bke2lB/faOeQ8eQhtZorf7ozFepUhqGTXRx90ZEg')
+rsconnect::deployApp('C:/Users/amelcher/Desktop/School/Classes/Winter 2019/ESM 244 - Adv Data Analysis/App/GoT_shiny_app')
+
